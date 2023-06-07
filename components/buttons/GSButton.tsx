3e66a27b69a302f5a12ee1ea3c5da1757eeaa783
diposs <onakunle.dipo@gmail.com>
2023-06-07 13:26:13 +0100

    Update GSButton.tsx

--- components/buttons/GSButton.tsx
+++ components/buttons/GSButton.tsx
@@ -5,7 +5,15 @@ export const GsButton = ({ ...rest }) =>  {
     const { classes } = useStyles();
     return (
     <Group className={classes.links}>
-        <Button radius="xl" h={30}  { ...rest }>GetStarted</Button>
+        <Button radius="xl" h={40}  { ...rest }>GetStarted</Button>
+    </Group>
+    );
+}
+export const GsLogoutButton = ({ ...rest }) =>  {
+    const { classes } = useStyles();
+    return (
+    <Group className={classes.links}>
+        <Button radius="md" h={40}  { ...rest }>LogOut</Button>
     </Group>
     );
 }
