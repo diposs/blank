d62fec4851bf24e2b279eeabd0afd1861769b1b6
diposs <onakunle.dipo@gmail.com>
2023-06-07 09:36:02 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { MantineProvider, createEmotionCache } from '@mantine/core';
 import { PolybaseProvider,AuthProvider } from "@polybase/react";
 import { Polybase } from "@polybase/client";
 import { Auth } from "@polybase/auth";
+import {FirstHeader} from '../components/header/header1';
 
 const polybase = new Polybase({defaultNamespace: process.env.NEXT_PUBLIC_DB,}); 
 const auth = typeof window !== "undefined" ? new Auth() : null;
@@ -25,6 +26,7 @@ export default function App({ Component, pageProps }: AppProps) {
      >
       <PolybaseProvider polybase={polybase}>
        <AuthProvider auth={auth!} polybase={polybase}>
+        <FirstHeader/>
          <Component {...pageProps} />
        </AuthProvider>
       </PolybaseProvider>
