fc947cec3c142e1bd3b7d8723b3e180032786c7e
diposs <onakunle.dipo@gmail.com>
2023-06-07 09:49:29 +0100

    Update skell.tsx

--- components/Text/skell.tsx
+++ components/Text/skell.tsx
@@ -1,11 +1,5 @@
 import { Skeleton } from '@mantine/core';
 
 export function Skel() {
-  return (
-    <>
-      <Skeleton height={8} radius="xl" />
-      <Skeleton height={8} mt={6} radius="xl" />
-      <Skeleton height={8} mt={6} width="70%" radius="xl" />
-    </>
-  );
+  return (<Skeleton height={8} width="15dvw" radius="xl" />);
 }
