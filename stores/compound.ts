de8cd34a81b2a6ef0d609df42859f0994468e839
diposs <onakunle.dipo@gmail.com>
2023-06-09 00:10:20 +0100

    Update compound.ts

--- stores/compound.ts
+++ stores/compound.ts
@@ -3,10 +3,10 @@ import { StateCreator } from 'zustand'
 export interface StoreInterface3 {
   inUser: boolean
   updateinUser: (inUser: boolean) => void
-  pKey: Uint8Array| string | null | undefined
-  pvKey: Uint8Array | string | null | undefined
-  updatepKey: (pKey: Uint8Array | string | null | undefined) => void
-  updatepvKey: (pvKey: Uint8Array | string | null | undefined) => void
+  pKey: Uint8Array | null | undefined
+  pvKey: Uint8Array | null | undefined
+  updatepKey: (pKey: Uint8Array | null | undefined) => void
+  updatepvKey: (pvKey: Uint8Array | null | undefined) => void
 }
 
 export const createcompound: StateCreator<StoreInterface3> = (set, get) => ({
